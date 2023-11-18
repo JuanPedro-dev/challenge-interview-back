9dbfcac025263915624f958873fbd1694f48369a
Juan Pedro <juanpedro.developer@gmail.com>
2023-11-18 20:34:31 -0300

    add corsFilter

--- src/main/java/com/Trionfini/challenge/ChallengeApplication.java
+++ src/main/java/com/Trionfini/challenge/ChallengeApplication.java
@@ -2,12 +2,35 @@ package com.Trionfini.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ChallengeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeApplication.class, args);
+	}
+
+	// Solución a problemas de CORS
+	@Bean
+	public CorsFilter corsFilter(){
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin",
+				"Content-Type", "Accept", "Authorization","Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin",
+				"Content-Type", "Accept", "Authorization","Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "OPTIONS", "HEAD"));
+		UrlBasedCorsConfigurationSource urlBasedCordsConfigurationsSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCordsConfigurationsSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCordsConfigurationsSource);
+		// Importar desde => import org.springframework.web.filter.CorsFilter;
 	}
 
 }
